88354e99070e5e3829692d6781f3bd7cb41266c7
Nexiakk <90979509+Nexiakk@users.noreply.github.com>
2025-07-04 23:56:12 +0200

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css"; // Kluczowy import dla globalnych stylów i Tailwind
-import { LoadingProvider } from "./contexts/LoadingContext"; // Import the provider
 import { ConfigProvider } from "antd";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LoadingProvider>
-      <ConfigProvider
-        theme={{
-          token: {
-            // The font family you set earlier
-            fontFamily: "'Plus Jakarta Sans', sans-serif",
+    <ConfigProvider
+      theme={{
+        token: {
+          // The font family you set earlier
+          fontFamily: "'Plus Jakarta Sans', sans-serif",
 
-            // ADD THIS LINE to shorten the animation duration
-            motionDurationMid: "0.1s",
-          },
-        }}
-      >
-        <App />
-      </ConfigProvider>
-    </LoadingProvider>
+          // ADD THIS LINE to shorten the animation duration
+          motionDurationMid: "0.1s",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
